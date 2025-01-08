04008314079759f380a877cab86ee9bc8905d07b
RahidAmin <rahidamin19@gmail.com>
2025-01-08 10:20:48 +0600

    crud added

---
+++ ___jwt__token.js
@@ -0,0 +1,14 @@
+/**
+ * ------------------
+ * make api secure
+ * ------------------
+ * 
+ * concept:
+ * 1.assign two tokens for each person(access token,refresh token)
+ * 2.token contains:user identification(email,role,etc).valid for a shorter duration.
+ * 3.refresh token is used to recreate an access token that was expired.
+ * 4.if refresh is invalid then log out the user.
+ * 
+ * 
+ * 
+ * **/
